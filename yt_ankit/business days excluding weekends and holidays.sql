54c34554b4e4ec31764ea17f790f848a5d35c93c
Tresa Varghese <tresageorgev@gmail.com>
2022-08-16 15:34:59 -0700

    Update business days excluding weekends and holidays.sql

--- yt_ankit/business days excluding weekends and holidays.sql
+++ yt_ankit/business days excluding weekends and holidays.sql
@@ -1,3 +1,5 @@
+-- 
+
 create database yt_ankit;
 use yt_ankit;
 
@@ -51,7 +53,7 @@ from tickets left join holidays
 group by ticket_id;
 
 
-  with a as (
+ with a as (
  select *, datediff(resolved_date, create_date) actual_diff,
 	floor(datediff(resolved_date, create_date) / 7) num_weeks
  from tickets),
@@ -59,6 +61,7 @@ group by ticket_id;
     select ticket_id, count(holiday_date) num_holidays
 	from tickets left join holidays
 		on holiday_date between create_date and resolved_date
+        and 
 	group by ticket_id
     )
  select *,
@@ -68,3 +71,44 @@ group by ticket_id;
 	end as op
  from a inner join holidays h on a.ticket_id = h.ticket_id;
  
+ -- Additional challenge
+ -- Insert a holiday that is a weekend. 
+ insert into holidays values
+('2022-08-14','Onam');
+
+select holiday_date, dayofweek(holiday_date) from holidays;
+
+with a as (
+ select *, datediff(resolved_date, create_date) actual_diff,
+	floor(datediff(resolved_date, create_date) / 7) num_weeks
+ from tickets),
+	holidays as (
+    select ticket_id, count(holiday_date) num_holidays
+	from tickets left join holidays
+		on holiday_date between create_date and resolved_date
+        and dayofweek(holiday_date) not in (1, 7)
+	group by ticket_id
+    )
+ select *,
+	case when dayofweek(create_date) = 1 then actual_diff - (num_weeks * 2) - 1 - num_holidays
+		when dayofweek(create_date) = 7 then actual_diff - (num_weeks * 2) - 2 - num_holidays
+        else actual_diff - (num_weeks * 2) - num_holidays
+	end as op
+ from a inner join holidays h on a.ticket_id = h.ticket_id;
+ 
+ -- assumption: create_date is never a weekend.
+ 
+ select *,
+	datediff(resolved_date, create_date) num_days,
+    weekofyear(resolved_date) - weekofyear(create_date) num_weeks
+ from tickets;
+ 
+ select *,
+	num_days - (num_weeks * 2) - num_holidays num_business_days
+ from (
+  select ticket_id, create_date, resolved_date,
+	datediff(resolved_date, create_date) num_days, weekofyear(resolved_date) - weekofyear(create_date) num_weeks,
+    count(holiday_date) num_holidays    
+ from tickets left join holidays on holiday_date between create_date and resolved_date
+	and dayofweek(holiday_date) not in (1, 7)
+ group by 1, 2, 3, 4) A;
